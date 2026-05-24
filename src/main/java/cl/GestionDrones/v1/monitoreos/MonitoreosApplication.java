28fe01b02e1c032c7bef6fdc826ebb7bd1ef5826
Lilyennd <victoriariffo21@gmail.com>
2026-05-24 13:38:10 -0400

    inicio

---
+++ src/main/java/cl/GestionDrones/v1/monitoreos/MonitoreosApplication.java
@@ -0,0 +1,13 @@
+package cl.GestionDrones.v1.monitoreos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoreosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoreosApplication.class, args);
+	}
+
+}
